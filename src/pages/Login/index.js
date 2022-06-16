43d27cc47f066e2b5ad916a28e093ce162502a9e
zqq <544520183@qq.com>
2022-06-16 17:39:47 +0800

    layout

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,77 +1,83 @@
-import './index.css'
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
-import React from 'react';
-import { useNavigate } from 'react-router-dom'
-import request from '@/utils/request'
+import "./index.css";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import request from "@/utils/request";
 function Login() {
-    const navigate = useNavigate();
-    const onFinish = (values) => {
-        console.log(values);
-        request({
-          method: "GET",
-          url: `/login`,
-          params: values,
-        })
-          .then((res) => {
-            // console.log(res, "res");
-            alert(res)
-            navigate('/layout')
-          })
-          .catch((err) => {
-            alert(err);
-          });
-      };
-    return (
-        <div className="login">
-            <div className="login-win">
-            <div className="login-logo">
-                LOGO
-            </div>
-            <div className="login-form">
-            <Form
-                name="normal_login"
-                className="login-form"
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                >
-                <Form.Item
-                    name="username"
-                    rules={[{ required: true, message: 'Please input your Username!'}]}
-                >
-                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your Password!' }]}
-                >
-                    <Input
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
-                    </Form.Item>
+  const navigate = useNavigate();
+  const onFinish = (values) => {
+    request({
+      method: "POST",
+      url: `/login`,
+      params: values,
+    })
+      .then((res) => {
+        navigate("/layout");
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+  return (
+    <div className="login">
+      <div className="login-win">
+        <div className="login-logo">LOGO</div>
+        <div className="login-form">
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
 
-                    <a className="login-form-forgot" href="">
-                    Forgot password
-                    </a>
-                </Form.Item>
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+            </Form.Item>
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                    </Button>
-                    Or <a href="">register now!</a>
-                </Form.Item>
-                </Form>
-            </div>
-            </div>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </Form.Item>
+          </Form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
